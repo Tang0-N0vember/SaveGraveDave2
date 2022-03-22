1f28de128b56816a1921990c986d349d6a714b4d
BarneyPC <barnabas.tamas-nagy@gmx.at>
2022-03-22 20:31:48 +0100

    Added player+animtaion and graves

---
+++ Assets/Project/Grave.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grave : MonoBehaviour
+{
+    [SerializeField] private GameObject GraveOpen;
+    [SerializeField] private GameObject GraveClosed;
+    [SerializeField] private bool isGraveOpne=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GraveOpen.SetActive(isGraveOpne?true:false);
+        GraveClosed.SetActive(isGraveOpne ? false : true);
+    }
+}
